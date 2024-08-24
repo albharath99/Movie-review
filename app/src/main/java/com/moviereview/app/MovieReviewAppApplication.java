5203f31e1d1784cc3c9a61636629192d1bc64f73
Bharath A L <albharath99@gmail.com>
2024-08-24 10:09:20 +0530

    Initial commit

---
+++ app/src/main/java/com/moviereview/app/MovieReviewAppApplication.java
@@ -0,0 +1,13 @@
+package com.moviereview.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReviewAppApplication.class, args);
+	}
+
+}
